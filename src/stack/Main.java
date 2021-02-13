f95cf4eb5f02285e464c85fdd774f8a65b21ccfd
parwani1him225 <himanshu.parwani8@gmail.com>
2021-02-13 11:13:34 +0530

    stack-2

--- src/stack/Main.java
+++ src/stack/Main.java
@@ -1,8 +1,26 @@
 package stack;
 
+import arrays.Array;
+
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println(Util.isBalancedParenthesis("{{}}}{}"));
+        System.out.println(Util.resolvedDirectoryStructure("/a/b/../c"));
+        Util.nextGreaterElement(Arrays.asList(1,2,13,4,6,16,0));
     }
 }
+
+//1,2,13,4,6,16,0
+//stack - 16 ,0
+//1 ---> 2
+//2 ----> 13
+//4 ---> 6
+//6 ---> 16
+//13 ---> 16
+//16 ---> -1
+//0 ----> -1
+
+//o(n)
--- src/stack/Util.java
+++ src/stack/Util.java
@@ -1,5 +1,8 @@
 package stack;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Util {
 
     public static boolean isBalancedParenthesis(String parenthesis) {
@@ -22,8 +25,43 @@ public class Util {
     }
 
     public static String resolvedDirectoryStructure(String structure) {
-        //TODO:
-        return null;
+        String[] strings = structure.split("/");
+        Stack<String> stack = new Stack<>();
+        for(int i=0;i<strings.length;i++) {
+
+            if(strings[i].equals("..")) {
+                stack.pop();
+            } else if(strings[i].equals("")){
+                continue;
+            } else {
+                stack.push(strings[i]);
+            }
+        }
+        StringBuffer stringBuffer = new StringBuffer();
+        String str = "abc";
+        str = str + "zxy";
+        while (!stack.isEmpty()) {
+            stringBuffer.append(stack.pop()).append("/");
+        }
+        return stringBuffer.reverse().toString();
+    }
+
+    //1, 2, 13, 4, 6, 16, 0
+    //
+    public static void nextGreaterElement(List<Integer> list) {
+        Stack<Integer> stack = new Stack<>();
+        for(int a : list) {
+            while(!stack.isEmpty() && stack.peek() < a) {
+                int temp = stack.pop();
+                System.out.println(temp + "------->" + a);
+            }
+            stack.push(a);
+        }
+        while (!stack.isEmpty()) {
+            int temp = stack.pop();
+            System.out.println(temp + "------->" + -1);
+        }
+
     }
 
 }
