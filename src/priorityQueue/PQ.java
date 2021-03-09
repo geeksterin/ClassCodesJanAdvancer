72ccf76b3eabeb62f987924fadf1641e92b9d3e1
Aman <amankrsingh.2012@gmail.com>
2021-03-09 22:23:54 +0530

    mergeKsorted Lists

--- src/priorityQueue/PQ.java
+++ src/priorityQueue/PQ.java
@@ -17,8 +17,15 @@ public class PQ {
     // int[] arr = {1,23,12,9,30,2,50};
     // printKlargestBetter(arr,2);
 
-    int[] arr ={30,10,40,20,50,70,80,60,110,90,100,120};
-    almostSorted(arr,2);
+    // int[] arr ={30,10,40,20,50,70,80,60,110,90,100,120};
+    // almostSorted(arr,2);
+
+    ArrayList<ArrayList<Integer>> lists = new ArrayList<>();
+    lists.add(new ArrayList<>(Arrays.asList(10,20,30,40,50)));
+    lists.add(new ArrayList<>(Arrays.asList(5,10,15,20,25)));
+    lists.add(new ArrayList<>(Arrays.asList(1,2,3,4,5)));
+    lists.add(new ArrayList<>(Arrays.asList(3,6,9,12)));
+    mergeKsortedLists(lists);
    } 
 
 //    Tc- nlogn, Sc - O(n)
@@ -79,6 +86,40 @@ public static void printKlargestBetter(int[] arr, int k){
 
         for(int val:arr){
             System.out.println(val);
+        }
+    }
+
+    static class pair implements Comparable<pair>{
+        int lIdx;
+        int dataIdx;
+        int value;
+
+        @Override
+        public int compareTo(pair o){
+            return this.value - o.value;
+        }
+    }
+    public static void mergeKsortedLists(ArrayList<ArrayList<Integer>> lists){
+        PriorityQueue<pair> pq = new PriorityQueue<>();
+        for(int i=0;i<lists.size();i++){
+            pair p = new pair();
+
+            p.dataIdx = 0;
+            p.lIdx = i;
+            p.value = lists.get(i).get(0);
+            pq.add(p);
+        }
+
+        while(pq.size()>0){
+            pair p = pq.remove();
+            System.out.print(p.value+" ");
+            if(p.dataIdx<lists.get(p.lIdx).size()-1){
+                pair p2 = new pair();
+                p2.dataIdx = p.dataIdx+1;
+                p2.lIdx = p.lIdx;
+                p2.value = lists.get(p.lIdx).get(p2.dataIdx);
+                pq.add(p2);
+            }
         }
     }
 }
