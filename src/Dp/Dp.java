125d4d41e00f2023ba29a5b23a928cb31a0b2381
Aman <amankrsingh.2012@gmail.com>
2021-03-20 20:57:32 +0530

    Added GoldMine dp

--- src/Dp/Dp.java
+++ src/Dp/Dp.java
@@ -3,8 +3,8 @@ import java.util.*;
 public class Dp {
     public static void main(String[] args) {
         // long startTime = System.currentTimeMillis();
-        int n = 5;
-        int[] strg = new int[n + 1];
+        // int n = 5;
+        // int[] strg = new int[n + 1];
         // System.out.println(fibBtr(n,strg));
         // long endTime = System.currentTimeMillis();
 
@@ -13,8 +13,14 @@ public class Dp {
         // System.out.println(climbDown(n));
         // System.out.println(climbDownMem(n, strg));
         // System.out.println(climbDownTabulation(n));
-        System.out.println(countMazePath(2,2));
-        System.out.println(countMazePath2(2,2));
+        // System.out.println(countMazePath(2,2));
+        // System.out.println(countMazePath2(2,2));
+
+        int[][] mine = {{10, 33, 13, 15},
+        {22, 21, 04, 1},
+        {5, 0, 2, 3},
+        {0, 6, 14, 2}};
+        System.out.println(goldMine(mine));
     }
 
     public static int fib(int n) {
@@ -121,5 +127,39 @@ public class Dp {
         }
 
         return strg[dr][dc];
+    }
+
+    public static int goldMine(int[][] gold){
+        int rowMax = gold.length -1;
+        int colMax = gold[0].length -1;
+        int[][] costs = new int[rowMax+1][colMax+1];
+
+        for(int i=0;i<=rowMax;i++){
+            costs[i][colMax] = gold[i][colMax];
+        }
+
+        for(int j=colMax-1;j>=0;j--){
+            for(int i=0;i<=rowMax;i++){
+                if(i==0){
+                    int max = Math.max(costs[i+1][j+1],costs[i][j+1]);
+                    costs[i][j] = gold[i][j]+max;
+                }else if(i==rowMax){
+                    int max = Math.max(costs[i-1][j+1],costs[i][j+1]);
+                    costs[i][j] = gold[i][j]+max;
+                }else{
+                    int max = Math.max(costs[i+1][j+1],Math.max(costs[i-1][j+1],costs[i][j+1]));
+                    costs[i][j] = gold[i][j]+max;
+                }
+            }
+        }
+
+        int ans = Integer.MIN_VALUE;
+        for(int i=0;i<=rowMax;i++){
+            if(costs[i][0]>ans){
+                ans = costs[i][0];
+            }
+        }
+
+        return ans;
     }
 }
