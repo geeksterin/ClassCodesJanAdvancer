5b653873d06b38c1146c76d8ba083b422fffc904
parwani1him225 <himanshu.parwani8@gmail.com>
2021-03-31 21:36:47 +0530

    feat: greedy approach

--- src/greedy/Main.java
+++ src/greedy/Main.java
@@ -6,6 +6,7 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        Utility.activitySelection(Arrays.asList(new Pair(10, 20), new Pair<>(5, 40) ,new Pair<>(20, 30)));
+       Utility.activitySelection(Arrays.asList(new Pair(10, 20), new Pair<>(5, 40) ,new Pair<>(20, 30)));
+        System.out.println(Utility.ropeCuttingProblem(Arrays.asList(2, 3, 6, 8)));
     }
 }
--- src/greedy/Utility.java
+++ src/greedy/Utility.java
@@ -5,14 +5,14 @@ import util.Pair;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Queue;
 
 public class Utility {
 
     public static void activitySelection(List<Pair<Integer, Integer>> activities) {
         Collections.sort(activities,
                 Comparator.comparingInt(Pair::getSecond));
-
-
         System.out.println(activities.get(0));
         for(int i=1;i<activities.size();i++) {
 
@@ -22,5 +22,18 @@ public class Utility {
                 System.out.println(current);
             }
         }
+    }
+    public static int ropeCuttingProblem(List<Integer> ropes) {
+        Queue<Integer> queue = new PriorityQueue<>();
+        queue.addAll(ropes);
+        int cost = 0;
+        while (queue.size() != 1) {
+            int first = queue.poll();
+            int second = queue.poll();
+            int resultant = first + second;
+            cost = cost + resultant;
+            queue.add(resultant);
+        }
+        return cost;
     }
 }
